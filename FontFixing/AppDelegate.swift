9e3b55fcdbd1fe5c2511678ef49a8b4278c60d04
Sean Rich <sean@snowfort.software>
2022-03-15 08:27:13 -0400

    Problem still exists without the text storage subclass, so I removed it

--- FontFixing/AppDelegate.swift
+++ FontFixing/AppDelegate.swift
@@ -2,20 +2,19 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     @IBOutlet var window: NSWindow!
     @IBOutlet var textView: NSTextView!
 
 
     override func awakeFromNib() {
-        textView.layoutManager?.replaceTextStorage(Storage())
         textView.string = "tes⌘ting"
         textView.font = systemFont
     }
     
     @IBAction func makeItalic(sender: Any) {
         let italicFont = NSFontManager.shared.convert(systemFont, toHaveTrait: .italicFontMask)
-        textView.textStorage?.addAttribute(.font, value: italicFont, range: NSRange(location: 3, length: 1))
+        let commandRange = NSRange(location: 3, length: 1)
+        textView.textStorage?.addAttribute(.font, value: italicFont, range: commandRange)
     }
     
     var systemFont: NSFont {
